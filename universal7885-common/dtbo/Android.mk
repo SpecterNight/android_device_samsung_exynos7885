b8dc99d544b6ba63ba6fe94a9d426cb968cfacd5
roynatech2544 <whiteshell2544@naver.com>
2024-04-12 08:40:26 +0900

    fixup! universal7885: compile dtbo from the kernel rather than prebuilt

--- universal7885-common/dtbo/Android.mk
+++ universal7885-common/dtbo/Android.mk
@@ -7,6 +7,7 @@ DTBO_CFG := $(COMMON_PATH)/dtbo/$(TARGET_DEVICE).cfg
 
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 define build-dtboimage-target
     $(call pretty,"Target dtbo image: $(INSTALLED_DTBIMAGE_TARGET)")
     $(MKDTIMG) cfg_create $@ $(DTBO_CFG) -d $(DTBO_DIR)
@@ -24,17 +25,22 @@ INSTALLED_DTBIMAGE_TARGET := $(PRODUCT_OUT)/eureka_dtbo.img
 	
 $(INSTALLED_DTBIMAGE_TARGET): $(PRODUCT_OUT)/kernel $(MKDTBOIMG)
 >>>>>>> 5bf5dfd (universal7885: dtbo: fix race condition)
+=======
+INSTALLED_DTBOIMAGE_TARGET := $(PRODUCT_OUT)/eureka_dtbo.img
+	
+$(INSTALLED_DTBOIMAGE_TARGET): $(PRODUCT_OUT)/kernel $(MKDTIMG)
+>>>>>>> 3b8b2da (fixup! universal7885: compile dtbo from the kernel rather than prebuilt)
 	$(call pretty,"Target dtbo image: $(INSTALLED_DTBIMAGE_TARGET)")
 	$(hide) echo "Building eureka_dtbo.img"
 	$(MKDTIMG) cfg_create $@ $(DTBO_CFG) -d $(DTBO_DIR)
-	$(hide) $(call assert-max-image-size,$@,$(BOARD_DTBIMAGE_PARTITION_SIZE),raw)
+#	$(hide) $(call assert-max-image-size,$@,$(BOARD_DTBOIMAGE_PARTITION_SIZE),raw)
 	$(hide) chmod a+r $@
 <<<<<<< HEAD
 >>>>>>> 8c104e9 (fixup! universal7885: compile dtbo from the kernel rather than prebuilt)
 =======
 >>>>>>> 5bf5dfd (universal7885: dtbo: fix race condition)
 	
-.PHONY: dtbimage
-dtbimage: $(INSTALLED_DTBIMAGE_TARGET)
+.PHONY: dtboimage
+dtboimage: $(INSTALLED_DTBIMAGE_TARGET)
 
-INSTALLED_RADIOIMAGE_TARGET += $(INSTALLED_DTBIMAGE_TARGET)
+INSTALLED_RADIOIMAGE_TARGET += $(INSTALLED_DTBOIMAGE_TARGET)
