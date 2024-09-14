bfe4b77980371287a59c05ec1034c40810bff95d
Jainer Pinta <36719078+SpecterNight@users.noreply.github.com>
2024-09-13 19:43:00 -0500

    Update vendorsetup.sh

--- universal7885-common/vendorsetup.sh
+++ universal7885-common/vendorsetup.sh
@@ -1,19 +1,45 @@
+#!/bin/bash
 UNIVERSAL="device/samsung/universal7885-common"
 FM_PATH="packages/apps/FMRadio"
-if [ -e ~/.git-credentials ]; then
-	MODE="https://"
-	SPERATOR="/"
-else
-	MODE="git@"
-	SPERATOR=":"
-fi
-git clone --depth=1 "$MODE"github.com"$SPERATOR"eurekadevelopment/Eureka-Kernel-Exynos7885-Q-R-S-private.git -b R10.1-T_rom kernel/samsung/exynos7885
-git clone https://github.com/lineageos/android_hardware_samsung_slsi_libbt hardware/samsung_slsi/libbt -b lineage-18.1
-git clone https://github.com/lineageos/android_hardware_samsung_slsi_scsc_wifibt_wifi_hal.git hardware/samsung_slsi/scsc_wifibt/wifi_hal -b lineage-18.1
-git clone https://github.com/lineageos/android_hardware_samsung_slsi_scsc_wifibt_wpa_supplicant_lib hardware/samsung_slsi/scsc_wifibt/wpa_supplicant_lib -b lineage-18.1
-git clone https://github.com/lineageos/android_hardware_samsung hardware/samsung -b lineage-18.1
-git clone --depth=1 https://github.com/eurekadevelopment/android_vendor_samsung_exynos7885.git -b master vendor/samsung
-git clone https://github.com/LineageOS/android_hardware_samsung_nfc hardware/samsung/nfc -b lineage-18.1
+
+echo 'Starting Cloning repos for exynos7885'
+echo 'Cloning Kernel tree [1/6]'
+# Kernel for exynos7885
+rm -rf kernel/samsung/exynos7885
+
+git clone https://github.com/eurekadevelopment/Eureka-Kernel-Exynos7885-Q-R-S.git -b R15_rom kernel/samsung/exynos7885
+
+echo 'Cloning Device Tree [2/6]'
+# Device tree for exynos7885
+rm -rf device/samsung
+
+git clone https://github.com/SpecterNight/android_device_samsung_exynos7885.git -b android-11 device/samsung
+
+cho 'Cloning Vendor Trees [3/6]'
+# Vendor blobs for exynos7885
+rm -rf vendor/samsung
+
+git clone https://github.com/eurekadevelopment/android_vendor_samsung_exynos7885.git -b android-12.1 vendor/samsung
+
+echo 'Cloning Hardware Samsung [4/6]'
+# Hardware OSS parts for Samsung
+mv hardware/samsung/nfc .
+rm -rf hardware/samsung
+git clone https://github.com/lineageos/android_hardware_samsung -b lineage-18.1 hardware/samsung
+mv nfc hardware/samsung
+
+echo 'Cloning Lineage-CP [6/6]'
+# Lineage-CP
+rm -rf hardware/samsung_slsi/libbt
+rm -rf hardware/samsung_slsi/scsc_wifibt/wifi_hal
+rm -rf hardware/samsung_slsi/scsc_wifibt/wpa_supplicant_lib
+
+git clone https://github.com/lineageos/android_hardware_samsung_slsi_libbt -b lineage-18.1 hardware/samsung_slsi/libbt
+git clone https://github.com/lineageos/android_hardware_samsung_slsi_scsc_wifibt_wifi_hal -b lineage-18.1 hardware/samsung_slsi/scsc_wifibt/wifi_hal
+git clone https://github.com/lineageos/android_hardware_samsung_slsi_scsc_wifibt_wpa_supplicant_lib -b lineage-18.1 hardware/samsung_slsi/scsc_wifibt/wpa_supplicant_lib
+
+echo 'Completed, Now proceeding to lunch'
+
 if test -f ${UNIVERSAL}/vendor_name; then
 	rm ${UNIVERSAL}/vendor_name
 fi
@@ -34,3 +60,7 @@ if [ -d "$FM_PATH" ]; then
 	rm -Rf $FM_PATH
 fi
 
+if [ ! -e .repo/local_manifests/eureka_deps.xml ]; then
+	git clone https://github.com/iamrh1819/local_manifests .repo/local_manifests
+	echo "Run repo sync again"
+fi
