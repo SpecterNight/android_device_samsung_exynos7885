ec5e9ff33f553d2177199d5d2067312de6bbcedf
SpecterNight <jainerpinta25@gmail.com>
2024-09-15 21:05:27 -0500

    fixup! universal7885: compile dtbo from the kernel rather than prebuilt

--- universal7885-common/dtbo/Android.mk
+++ universal7885-common/dtbo/Android.mk
@@ -5,40 +5,14 @@ KERNEL_OUT := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ
 DTBO_DIR   := $(KERNEL_OUT)/arch/$(KERNEL_ARCH)/boot/dts/exynos/dtbo
 DTBO_CFG := $(COMMON_PATH)/dtbo/$(TARGET_DEVICE).cfg
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-define build-dtboimage-target
-    $(call pretty,"Target dtbo image: $(INSTALLED_DTBIMAGE_TARGET)")
-    $(MKDTIMG) cfg_create $@ $(DTBO_CFG) -d $(DTBO_DIR)
-    $(hide) chmod a+r $@
-endef
-
-$(INSTALLED_DTBIMAGE_TARGET): $(MKDTIMG) $(INSTALLED_KERNEL_TARGET)
-	$(build-dtboimage-target)
-=======
-INSTALLED_DTBIMAGE_TARGET := $(PRODUCT_OUT)/eureka_dtbo.img
-	
-$(INSTALLED_DTBIMAGE_TARGET): $(PRODUCT_OUT)/kernel $(MKDTIMG)
-=======
-INSTALLED_DTBIMAGE_TARGET := $(PRODUCT_OUT)/eureka_dtbo.img
-	
-$(INSTALLED_DTBIMAGE_TARGET): $(PRODUCT_OUT)/kernel $(MKDTBOIMG)
->>>>>>> 5bf5dfd (universal7885: dtbo: fix race condition)
-=======
 INSTALLED_DTBOIMAGE_TARGET := $(PRODUCT_OUT)/eureka_dtbo.img
 	
 $(INSTALLED_DTBOIMAGE_TARGET): $(PRODUCT_OUT)/kernel $(MKDTIMG)
->>>>>>> 3b8b2da (fixup! universal7885: compile dtbo from the kernel rather than prebuilt)
 	$(call pretty,"Target dtbo image: $(INSTALLED_DTBIMAGE_TARGET)")
 	$(hide) echo "Building eureka_dtbo.img"
 	$(MKDTIMG) cfg_create $@ $(DTBO_CFG) -d $(DTBO_DIR)
 #	$(hide) $(call assert-max-image-size,$@,$(BOARD_DTBOIMAGE_PARTITION_SIZE),raw)
 	$(hide) chmod a+r $@
-<<<<<<< HEAD
->>>>>>> 8c104e9 (fixup! universal7885: compile dtbo from the kernel rather than prebuilt)
-=======
->>>>>>> 5bf5dfd (universal7885: dtbo: fix race condition)
 	
 .PHONY: dtboimage
 dtboimage: $(INSTALLED_DTBIMAGE_TARGET)
