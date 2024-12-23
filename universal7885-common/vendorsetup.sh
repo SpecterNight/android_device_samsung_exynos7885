ae41fae99c650e6cbe6922be053babfbb70fa274
Sebastian Pinta <36719078+SpecterNight@users.noreply.github.com>
2024-12-22 22:54:24 -0500

    Update vendorsetup.sh

--- universal7885-common/vendorsetup.sh
+++ universal7885-common/vendorsetup.sh
@@ -3,31 +3,32 @@ UNIVERSAL="device/samsung/universal7885-common"
 FM_PATH="packages/apps/FMRadio"
 
 echo 'Starting Cloning repos for exynos7885'
-echo 'Cloning Kernel tree [1/6]'
+echo 'Cloning Kernel tree [1/7]'
 # Kernel for exynos7885
 rm -rf kernel/samsung/exynos7885
 
 git clone https://github.com/eurekadevelopment/Eureka-Kernel-Exynos7885-Q-R-S.git -b R15_rom kernel/samsung/exynos7885
 
-echo 'Cloning Device Tree [2/6]'
+echo 'Cloning Device Tree [2/7]'
 # Device tree for exynos7885
 rm -rf device/samsung
 
 git clone https://github.com/SpecterNight/android_device_samsung_exynos7885.git -b android-11 device/samsung
 
-cho 'Cloning Vendor Trees [3/6]'
+cho 'Cloning Vendor Trees [3/7]'
 # Vendor blobs for exynos7885
 rm -rf vendor/samsung
 
 git clone https://github.com/SpecterNight/android_vendor_samsung_exynos7885.git -b master vendor/samsung
 
-echo 'Cloning Hardware Samsung [4/6]'
+echo 'Cloning Hardware Samsung [4/7]'
 # Hardware OSS parts for Samsung
+mv hardware/samsung/nfc .
 rm -rf hardware/samsung
-git clone https://github.com/lineageos/android_hardware_samsung -b lineage-18.1 hardware/samsung
-git clone https://github.com/LineageOS/android_hardware_samsung_nfc -b lineage-18.1 hardware/samsung/nfc
+git clone https://github.com/Roynas-Android-Playground/android_hardware_samsung -b lineage-18.1 hardware/samsung
+mv nfc hardware/samsung
 
-echo 'Cloning Lineage-CP [6/6]'
+echo 'Cloning Lineage-CP [6/7]'
 # Lineage-CP
 rm -rf hardware/samsung_slsi/libbt
 rm -rf hardware/samsung_slsi/scsc_wifibt/wifi_hal
@@ -36,6 +37,11 @@ rm -rf hardware/samsung_slsi/scsc_wifibt/wpa_supplicant_lib
 git clone https://github.com/lineageos/android_hardware_samsung_slsi_libbt -b lineage-18.1 hardware/samsung_slsi/libbt
 git clone https://github.com/lineageos/android_hardware_samsung_slsi_scsc_wifibt_wifi_hal -b lineage-18.1 hardware/samsung_slsi/scsc_wifibt/wifi_hal
 git clone https://github.com/lineageos/android_hardware_samsung_slsi_scsc_wifibt_wpa_supplicant_lib -b lineage-18.1 hardware/samsung_slsi/scsc_wifibt/wpa_supplicant_lib
+
+echo 'Cloning Samsung_Slsi [7/7]'
+# SLSI Sepolicy
+rm -rf device/samsung_slsi/sepolicy
+git clone https://github.com/Roynas-Android-Playground/android_device_samsung_slsi_sepolicy -b lineage-22 device/samsung_slsi/sepolicy
 
 echo 'Completed, Now proceeding to lunch'
 
